2867d2e97e823dafecce8dd975b6763728d0694a
debshila <debshila@gmail.com>
2024-10-22 12:17:41 -0500

    Revert "Updated to include new speakers"
    This reverts commit 49ddcf758f42df8910a0e7373be51e6b1e2229f8.

--- 40a1d87829d76804.en.js
+++
@@ -1,2 +0,0 @@
-window['cmsg']['assets'] = window['cmsg']['assets'] || {};
-window['cmsg']['assets']["en"] = Object.assign(window['cmsg']['assets']["en"] || {}, {"3369":{"js":["5b3337d68427bbd1.strings.js"],"css":[]},"3499":{"js":["3ce99f5b9704eb27.strings.js"],"css":[]},"8528":{"js":["51cf4c6541ca2271.strings.js"],"css":[]}});
